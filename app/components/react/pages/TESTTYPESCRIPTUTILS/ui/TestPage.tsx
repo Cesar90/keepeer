4e5cd92eeb4d1f61ec77a0a6965a09de59b2109b
Cesar <ccordero.007@gmail.com>
2025-10-21 21:56:49 -0600

    Const annotation used to infer and Object's values as Literal Types

--- app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
+++ app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
@@ -184,6 +184,26 @@ type EventTypeIndex = Event["type"]
 
 type testsEventTypeIndex = [Expect<Equal<EventTypeIndex, "click" | "focus" | "keydown">>]
 
+export const programModeEnumMap = {
+    Group: "group",
+    ANNOUNCEMENT: "announcement",
+    ONE_ON_ONE: "1on1",
+    SELF_DIRECTED: "selfDirected",
+    PLANNED_ONE_ON_ONE: "planned1on1",
+    PLANNED_SELF_DIRECTED: "plannedSelfDirected"
+} as const
+
+export type GroupProgram = typeof programModeEnumMap["Group"]
+export type AnnouncementProgram = typeof programModeEnumMap["ANNOUNCEMENT"]
+export type OneOnOneProgram = typeof programModeEnumMap["ONE_ON_ONE"]
+export type SelfDirectedProgram = typeof programModeEnumMap["SELF_DIRECTED"]
+export type PlannedOneOnOneProgram = typeof programModeEnumMap["PLANNED_ONE_ON_ONE"]
+export type PlannedSelfDirectedProgram = typeof programModeEnumMap["PLANNED_SELF_DIRECTED"]
+
+type testObjectConst = [
+    Expect<Equal<GroupProgram, "group">>,
+]
+
 const LoginPage = () => {
     return (
         <div
