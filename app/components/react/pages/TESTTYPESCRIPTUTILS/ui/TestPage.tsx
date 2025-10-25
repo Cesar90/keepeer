cb884ab8e9e71a496ba98466744e07b0401e9176
Cesar <ccordero.007@gmail.com>
2025-10-25 16:12:51 -0600

    Passing Unions into Template Literals

--- app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
+++ app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
@@ -269,6 +269,27 @@ type DynamicRoutes = Extract<Routes, `${string}:${string}`>
 
 type testsDynamicRoutes = [Expect<Equal<DynamicRoutes, "/users/:id" | "/posts/:id">>]
 
+type BreadType = "rye" | "brown" | "white"
+
+type Filling = "cheese" | "ham" | "salami"
+
+type Sandwich = `${BreadType} sandwich with ${Filling}`
+
+type testSandwich = [
+    Expect<Equal<
+        Sandwich,
+        | "rye sandwich with cheese"
+        | "rye sandwich with ham"
+        | "rye sandwich with salami"
+        | "brown sandwich with cheese"
+        | "brown sandwich with ham"
+        | "brown sandwich with salami"
+        | "white sandwich with cheese"
+        | "white sandwich with ham"
+        | "white sandwich with salami"
+    >>
+]
+
 const LoginPage = () => {
     return (
         <div
