096faa385da839f77df71e4616157eadddcba79a
Cesar <ccordero.007@gmail.com>
2025-10-25 19:34:57 -0600

    Support Function Type Constrains with Variable Arguments

--- app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
+++ app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
@@ -1,6 +1,5 @@
-import { KeyboardEvent } from "react";
-import { S } from 'ts-toolbelt'
-import { P } from "ts-toolbelt/out/Object/_api";
+import { KeyboardEvent } from 'react';
+import { S } from 'ts-toolbelt';
 
 const myFunc = () => {
     return 'Hello';
@@ -86,83 +85,83 @@ type testTestingFramework = [
 
 /**
  * It's important to understand the terminology around unions:
- * 
+ *
  * One of the type declarations below is a union
  * One of the type declarations below is a discriminated union.
  * One of the type declarations below is an enum
- * 
+ *
  * Which is which?
 */
 
 // Discrimination union
 type A =
     | {
-        type: "a";
+        type: 'a';
         whatever: string
     }
     | {
-        type: "b",
+        type: 'b',
         something: string
     }
     | {
-        type: "c",
+        type: 'c',
         whoCares: string
     }
 
 const getUnion = (result: A) => {
-    if (result.type === "a") {
-        result.whatever
+    if (result.type === 'a') {
+        // result.whatever;
     }
-}
+};
 
 // B is a union
-type B = "a" | "b" | "c"
+type B = 'a' | 'b' | 'c'
 
 // C is an enum.
 enum C {
-    A = "a",
-    B = "b",
-    C = "c"
+    A = 'a',
+    B = 'b',
+    C = 'c'
 }
 
 export type Event =
     | {
-        type: "click";
+        type: 'click';
         event: MouseEvent
     }
     | {
-        type: "focus";
+        type: 'focus';
         event: FocusEvent;
     }
     | {
-        type: "keydown";
+        type: 'keydown';
         event: KeyboardEvent
     };
 
-type Fruit = "apple" | "banana" | "orange";
+type Fruit = 'apple' | 'banana' | 'orange';
 
-type BananaAndOrange = Extract<Fruit, "banana" | "orange">
+type BananaAndOrange = Extract<Fruit, 'banana' | 'orange'>
 
-type ClickEvent = Extract<Event, { type: "click" }>
+type ClickEvent = Extract<Event, { type: 'click' }>
 
-type testsClick = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>]
+type testsClick = [Expect<Equal<ClickEvent, { type: 'click'; event: MouseEvent }>>]
 
-type NoKeyDownEvents = Exclude<Event, { type: "keydown" }>
+type NoKeyDownEvents = Exclude<Event, { type: 'keydown' }>
 
 type testsClickExclude = [Expect<Equal<NoKeyDownEvents,
-    { type: "click"; event: MouseEvent; }
-    | { type: "focus"; event: FocusEvent; }>>]
+    { type: 'click'; event: MouseEvent; }
+    | { type: 'focus'; event: FocusEvent; }>>]
 
 const fakeDataDefaults = {
-    String: "Default string",
+    String: 'Default string',
     Int: 1,
     Float: 1.14,
     Boolean: true,
-    ID: "id",
+    ID: 'id',
     obj: {
-        String: "Default string",
-    }
-}
+        String: 'Default string',
+    },
+};
 
 type Example = typeof fakeDataDefaults['obj']['String']
 
@@ -182,28 +181,28 @@ type testFakeDataDefaults = [
     Expect<Equal<IDType, string>>,
 ]
 
-type EventTypeIndex = Event["type"]
+type EventTypeIndex = Event['type']
 
-type testsEventTypeIndex = [Expect<Equal<EventTypeIndex, "click" | "focus" | "keydown">>]
+type testsEventTypeIndex = [Expect<Equal<EventTypeIndex, 'click' | 'focus' | 'keydown'>>]
 
 export const programModeEnumMap = {
-    GROUP: "group",
-    ANNOUNCEMENT: "announcement",
-    ONE_ON_ONE: "1on1",
-    SELF_DIRECTED: "selfDirected",
-    PLANNED_ONE_ON_ONE: "planned1on1",
-    PLANNED_SELF_DIRECTED: "plannedSelfDirected"
-} as const
+    GROUP: 'group',
+    ANNOUNCEMENT: 'announcement',
+    ONE_ON_ONE: '1on1',
+    SELF_DIRECTED: 'selfDirected',
+    PLANNED_ONE_ON_ONE: 'planned1on1',
+    PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const;
 
-export type GroupProgram = typeof programModeEnumMap["GROUP"]
-export type AnnouncementProgram = typeof programModeEnumMap["ANNOUNCEMENT"]
-export type OneOnOneProgram = typeof programModeEnumMap["ONE_ON_ONE"]
-export type SelfDirectedProgram = typeof programModeEnumMap["SELF_DIRECTED"]
-export type PlannedOneOnOneProgram = typeof programModeEnumMap["PLANNED_ONE_ON_ONE"]
-export type PlannedSelfDirectedProgram = typeof programModeEnumMap["PLANNED_SELF_DIRECTED"]
+export type GroupProgram = typeof programModeEnumMap['GROUP']
+export type AnnouncementProgram = typeof programModeEnumMap['ANNOUNCEMENT']
+export type OneOnOneProgram = typeof programModeEnumMap['ONE_ON_ONE']
+export type SelfDirectedProgram = typeof programModeEnumMap['SELF_DIRECTED']
+export type PlannedOneOnOneProgram = typeof programModeEnumMap['PLANNED_ONE_ON_ONE']
+export type PlannedSelfDirectedProgram = typeof programModeEnumMap['PLANNED_SELF_DIRECTED']
 
 type testObjectConst = [
-    Expect<Equal<GroupProgram, "group">>,
+    Expect<Equal<GroupProgram, 'group'>>,
 ]
 
 // export type IndividualProgram = typeof programModeEnumMap[
@@ -215,7 +214,7 @@ type testObjectConst = [
 
 type ExmapleIndividualProgram = Exclude<
     keyof typeof programModeEnumMap,
-    "GROUP" | "ANNOUNCEMENT"
+    'GROUP' | 'ANNOUNCEMENT'
 >
 
 export type IndividualProgram = typeof programModeEnumMap[ExmapleIndividualProgram]
@@ -223,33 +222,33 @@ export type IndividualProgram = typeof programModeEnumMap[ExmapleIndividualProgr
 type testsIndividualProgram = [
     Expect<Equal<
         IndividualProgram,
-        "1on1" |
-        "selfDirected" |
-        "planned1on1" |
-        "plannedSelfDirected"
+        '1on1' |
+        'selfDirected' |
+        'planned1on1' |
+        'plannedSelfDirected'
     >>
 ]
 
 const frontendToBackEnumMap = {
-    singleModule: "SINGLE_MODULE",
-    multiModule: "MULTI_MODULE",
-    sharedModule: "SHARED_MODULE",
+    singleModule: 'SINGLE_MODULE',
+    multiModule: 'MULTI_MODULE',
+    sharedModule: 'SHARED_MODULE',
 } as const;
 
 type BackendModuleEnum = typeof frontendToBackEnumMap[keyof typeof frontendToBackEnumMap]
 
 type testFrontendToBackEnumMap = [
-    Expect<Equal<BackendModuleEnum, "SINGLE_MODULE" | "MULTI_MODULE" | "SHARED_MODULE">>
+    Expect<Equal<BackendModuleEnum, 'SINGLE_MODULE' | 'MULTI_MODULE' | 'SHARED_MODULE'>>
 ]
 
-const fruits = ["apple", "banana", "orange"] as const;
+const fruits = ['apple', 'banana', 'orange'] as const;
 
 type AppleOrBanana = typeof fruits[0 | 1];
 type FruitFruits = typeof fruits[number]
 
 type testsFruits = [
-    Expect<Equal<AppleOrBanana, "apple" | "banana">>,
-    Expect<Equal<Fruit, "apple" | "banana" | "orange">>
+    Expect<Equal<AppleOrBanana, 'apple' | 'banana'>>,
+    Expect<Equal<Fruit, 'apple' | 'banana' | 'orange'>>
 ]
 
 type RouteLiteral = `/${string}`
@@ -257,50 +256,50 @@ type RouteLiteral = `/${string}`
 export const goToRoute = (route: RouteLiteral) => { };
 
 // Should succeed
-goToRoute("/users");
-goToRoute("/")
-goToRoute("/admin/users")
+goToRoute('/users');
+goToRoute('/');
+goToRoute('/admin/users');
 
 // Should error
 // @ts-expect-error
-goToRoute("users/1")
+goToRoute('users/1');
 
-type Routes = "/users" | "/users/:id" | "/posts" | "/posts/:id";
+type Routes = '/users' | '/users/:id' | '/posts' | '/posts/:id';
 
 type DynamicRoutes = Extract<Routes, `${string}:${string}`>
 
-type testsDynamicRoutes = [Expect<Equal<DynamicRoutes, "/users/:id" | "/posts/:id">>]
+type testsDynamicRoutes = [Expect<Equal<DynamicRoutes, '/users/:id' | '/posts/:id'>>]
 
-type BreadType = "rye" | "brown" | "white"
+type BreadType = 'rye' | 'brown' | 'white'
 
-type Filling = "cheese" | "ham" | "salami"
+type Filling = 'cheese' | 'ham' | 'salami'
 
 type Sandwich = `${BreadType} sandwich with ${Filling}`
 
 type testSandwich = [
     Expect<Equal<
         Sandwich,
-        | "rye sandwich with cheese"
-        | "rye sandwich with ham"
-        | "rye sandwich with salami"
-        | "brown sandwich with cheese"
-        | "brown sandwich with ham"
-        | "brown sandwich with salami"
-        | "white sandwich with cheese"
-        | "white sandwich with ham"
-        | "white sandwich with salami"
+        | 'rye sandwich with cheese'
+        | 'rye sandwich with ham'
+        | 'rye sandwich with salami'
+        | 'brown sandwich with cheese'
+        | 'brown sandwich with ham'
+        | 'brown sandwich with salami'
+        | 'white sandwich with cheese'
+        | 'white sandwich with ham'
+        | 'white sandwich with salami'
     >>
 ]
 
-type Path = "Users/John/Documents/notes.txt"
+type Path = 'Users/John/Documents/notes.txt'
 
-type SplitPath = S.Split<Path, "/">
+type SplitPath = S.Split<Path, '/'>
 
 type testsPath = [
-    Expect<Equal<SplitPath, ["Users", "John", "Documents", "notes.txt"]>>
+    Expect<Equal<SplitPath, ['Users', 'John', 'Documents', 'notes.txt']>>
 ]
 
-type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`
+type TemplateLiteralKey = `${'user' | 'post' | 'comment'}${'Id' | 'Name'}`
 
 type ObjectOfKeys = Record<TemplateLiteralKey, string>
 
@@ -320,7 +319,7 @@ type testsObjectKeys = [
     >
 ]
 
-type EventUnion = `log_in` | "log_out" | "sign_up"
+type EventUnion = 'log_in' | 'log_out' | 'sign_up'
 
 type ObjectOfKeysEventUnion = Record<Uppercase<EventUnion>, string>
 
@@ -333,11 +332,11 @@ type testsEventUnion = [
 ]
 
 export const TypePurchases = {
-    firstSelfHostLicensePurchase: "first_purchase",
-    renewalSelfHost: "renewal_self",
-    monthlySubscription: "monthly_subscription",
-    annualSubscription: "annual_subscription"
-} as const
+    firstSelfHostLicensePurchase: 'first_purchase',
+    renewalSelfHost: 'renewal_self',
+    monthlySubscription: 'monthly_subscription',
+    annualSubscription: 'annual_subscription',
+} as const;
 
 type TypePurchasesValueOF = ValueOf<typeof TypePurchases>
 
@@ -351,7 +350,7 @@ type ReturnWhatIPassIn<T> = T
 
 type testReturnWhatIPassIn = [
     Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
-    Expect<Equal<ReturnWhatIPassIn<"1">, "1">>,
+    Expect<Equal<ReturnWhatIPassIn<'1'>, '1'>>,
     Expect<Equal<ReturnWhatIPassIn<true>, true>>,
     Expect<Equal<ReturnWhatIPassIn<false>, false>>,
     Expect<Equal<ReturnWhatIPassIn<null>, null>>,
@@ -371,10 +370,10 @@ type testsMaybe = [
 type AddRoutePrefix<TRoute extends string> = `/${TRoute}`
 
 type testsAddRoutePrefix = [
-    Expect<Equal<AddRoutePrefix<"">, "/">>,
-    Expect<Equal<AddRoutePrefix<"about">, "/about">>,
-    Expect<Equal<AddRoutePrefix<"about/team">, "/about/team">>,
-    Expect<Equal<AddRoutePrefix<"blog">, "/blog">>,
+    Expect<Equal<AddRoutePrefix<''>, '/'>>,
+    Expect<Equal<AddRoutePrefix<'about'>, '/about'>>,
+    Expect<Equal<AddRoutePrefix<'about/team'>, '/about/team'>>,
+    Expect<Equal<AddRoutePrefix<'blog'>, '/blog'>>,
     // @ts-expect-error
     AddRoutePrefix<boolean>,
     // @ts-expect-error
@@ -419,7 +418,17 @@ type testsShapeError = [
     >>
 ]
 
+type GetParametersAndReturnType<T extends (...args: any) => any> = {
+    params: Parameters<T>;
+    returnValue: ReturnType<T>;
+}
 
+type testsGetParametersAndReturnType = [
+    Expect<Equal<GetParametersAndReturnType<() => string>, { params: []; returnValue: string }>>,
+    Expect<Equal<GetParametersAndReturnType<(s: string) => string>, { params: [string]; returnValue: string }>>,
+    Expect<Equal<GetParametersAndReturnType<(s: string) => void>, { params: [string]; returnValue: void }>>,
+    Expect<Equal<GetParametersAndReturnType<(n: number, b: boolean) => number>, { params: [number, boolean]; returnValue: number }>>,
+]
 
 const LoginPage = () => {
     return (
