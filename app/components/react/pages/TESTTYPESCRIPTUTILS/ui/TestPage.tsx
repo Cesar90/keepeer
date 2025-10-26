0491bdc5ff0940382a97c112d62f763757ee076d
Cesar <ccordero.007@gmail.com>
2025-10-26 00:27:32 -0600

    Use infer with Generics to Extract Types From Arguments

--- app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
+++ app/components/react/pages/TESTTYPESCRIPTUTILS/ui/TestPage.tsx
@@ -483,6 +483,30 @@ type testsGetDataValues = [
     Expect<Equal<GetDataValues<{ data: { name: "hello"; age: 20 } }>, { name: "hello"; age: 20 }>>,
 ]
 
+interface MyComplexInterface<Event, Context, Name, Point> {
+    getEvent: () => Event;
+    getContext: () => Context;
+    getName: () => Name;
+    getPoint: () => Point;
+}
+
+type ExampleMyComplexInterface = MyComplexInterface<
+    "click",
+    "window",
+    "my-event",
+    { x: 12, y: 14 }
+>
+
+type GetPoint<T> = T extends MyComplexInterface<infer TClick, infer TWindow, infer TEvent, infer TPoint>
+    ? TPoint
+    : never;
+
+type Example2 = GetPoint<MyComplexInterface<1, 2, 3, 4>>
+
+type testsMyComplexInterface = [Expect<Equal<GetPoint<ExampleMyComplexInterface>, { x: 12, y: 14 }>>]
+
+
+
 const LoginPage = () => {
     return (
         <div
